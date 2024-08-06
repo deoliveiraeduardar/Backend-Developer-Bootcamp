091afbcdad937b55449fc2402f1c72ed0922b8ab
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2024-08-06 19:42:23 -0300

    Update server-2.js

--- node-http-server-aula/server-2.js
+++ node-http-server-aula/server-2.js
@@ -1,13 +1,23 @@
 const http = require('http');
 
 const host = 'localhost';
-const port = 8000;
+const port = 3000;
 
 const server = http.createServer(function (req, res) {
-  res.writeHead(205, { 'Set-Cookie': 'type=ninja' });
-  res.end('My second server');
+  res.setHeader('Content-Type', 'text/html');
+  res.writeHead(200);
+  res.end('<html><body><h1> Web server </h1></body></html>');
 });
 
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
+
+/* Abrir a pasta (folder) no vscode
+Abrir o terminal, que deve conter o caminho da pasta
+Digitar no terminal: npm init -y
+Digitar no terminal: nodemon titulo-do-arquivo.js (não funciona)
+Digitar: node server-2.js (nodemon ele nao reconhece)
+
+Digitar: curl http://localhost:3000/
+*/
